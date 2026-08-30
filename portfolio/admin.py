2383ad3ca4b48bb40bc8f2201730375062000eb4
karib shams <154616482+karibshams@users.noreply.github.com>
2026-08-30 15:12:15 +0600

    update the admin part

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Feedback, ChatLog
+from .models import Feedback, ChatLog, VisitorCount
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
@@ -10,3 +10,8 @@ class FeedbackAdmin(admin.ModelAdmin):
 class ChatLogAdmin(admin.ModelAdmin):
     list_display = ('user_message', 'timestamp')
     ordering = ('-timestamp',)
+
+@admin.register(VisitorCount)
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ('date', 'count')
+    ordering = ('-date',)
